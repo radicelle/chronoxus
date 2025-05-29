f6c385d6bc6b484e0f0f6cd299caac4c86dc85bc
manua <emmanuel.breton-belz@outlook.fr>
2025-05-29 09:12:35 +0200

    generate static

--- src/inputs.rs
+++ src/inputs.rs
@@ -1,14 +1,7 @@
-use chrono::{Datelike, Month};
+use chrono::{Datelike};
 use chrono::{Utc};
 use dioxus::prelude::*;
 use crate::calendar_grid::CalendarGrid;
-
-fn get_number_of_days(month_number: i32) -> u8 {
-    Month::try_from(month_number as u8)
-        .ok()
-        .map(|month| month.num_days(Utc::now().year()).expect("year not found"))
-        .expect("can't get number of days")
-}
 
 #[component]
 pub fn Inputs(selected_month: i32) -> Element {
